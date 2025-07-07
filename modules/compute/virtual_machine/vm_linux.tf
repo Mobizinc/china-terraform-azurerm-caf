dbc98f1c7e90500eed4362ff5650992cd6d9123e
Viacheslav Kuzmenko <60648640+onlyillusion@users.noreply.github.com>
2025-07-07 18:42:41 +0300

    temporarly disabling source_image_id

--- modules/compute/virtual_machine/vm_linux.tf
+++ modules/compute/virtual_machine/vm_linux.tf
@@ -208,7 +208,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   lifecycle {
     ignore_changes = [
       os_disk[0].name, #for ASR disk restores
-      admin_ssh_key
+      admin_ssh_key,
+      source_image_id #temporarly ignore
     ]
   }
 
